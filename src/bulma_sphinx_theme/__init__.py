dd5b4f7847c0de3baf01512627479bda04678aff
zc <syfhub@hotmail.com>
2023-05-24 13:50:13 +0800

    initial commit

--- src/bulma_sphinx_theme/__init__.py
+++ src/bulma_sphinx_theme/__init__.py
@@ -101,6 +101,10 @@ def update_and_remove_templates(
 ) -> None:
     template_sections = [
         "theme_footer",
+        "theme_article_top_left",
+        "theme_article_top_right",
+        "theme_article_bottom_left",
+        "theme_article_bottom_right",
     ]
 
     for section in template_sections:
