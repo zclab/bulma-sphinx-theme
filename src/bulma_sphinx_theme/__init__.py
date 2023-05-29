72e29fda4d857e5a246bcab7b4560a702610a17a
zc <syfhub@hotmail.com>
2023-05-30 00:19:30 +0800

    bump to dev0

--- src/bulma_sphinx_theme/__init__.py
+++ src/bulma_sphinx_theme/__init__.py
@@ -9,7 +9,7 @@ from sphinx.builders.html import StandaloneHTMLBuilder
 from sphinx.locale import get_translation
 from . import pygment, toctree, transforms, utils
 
-__version__ = "0.0.6"
+__version__ = "0.0.7.dev0"
 logger = logging.getLogger(__name__)
 MESSAGE_CATALOG_NAME = "bulmasphinxtheme"
 
